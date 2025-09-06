d5a5d1e98203c2187b06a8f1e29dfd106a796072
Vikram Rajput <vikramrajput1987@gmail.com>
2025-09-07 01:16:14 +0530

    first commit

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,13 +1,13 @@
 import { config } from "dotenv";
 import { defineConfig } from "drizzle-kit";
 config({ path: ".env" });
-
+const isProduction = process.env.NODE_ENV === "production";
 export default defineConfig({
   schema: "./lib/db/schema.ts",  // adjust if different
   out: "./lib/db/migrations",
   dialect: "postgresql",
   dbCredentials: {
-    url: process.env.DATABASE_URL!,
+    url: isProduction ? process.env.PRODUCTION_DATABASE_URL! : process.env.DATABASE_URL!,
   },
   verbose: true,
   strict: true,
