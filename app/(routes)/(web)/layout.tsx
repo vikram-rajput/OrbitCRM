846771997c8f3db9afea8d3774c65206c745006a
Vikram Rajput <vikramrajput1987@gmail.com>
2025-09-07 00:40:00 +0530

    updated

--- app/(routes)/(web)/layout.tsx
+++ app/(routes)/(web)/layout.tsx
@@ -1,16 +1,11 @@
 import React from "react";
 import { cn } from "@/lib/common/utils";
-import { Inter, Bricolage_Grotesque } from "next/font/google";
 import "./website.css";
 
 import { Navbar } from "@/components/layout/landing/navbar";
 import { ThemeProvider } from "@/components/layout/landing/theme-provider";
 
-const inter = Inter({ subsets: ["latin"] });
-const bricolageGrotesque = Bricolage_Grotesque({
-  subsets: ["latin"],
-  variable: "--bricolage-grotesque"
-});
+
 
 export default function RootLayout({
   children
@@ -21,9 +16,7 @@ export default function RootLayout({
     <html lang="en" suppressHydrationWarning className="scroll-smooth">
       <body
         className={cn(
-          "from-muted to-primary/5 min-h-screen bg-gradient-to-tl",
-          inter.className,
-          bricolageGrotesque.variable
+          "from-muted to-primary/5 min-h-screen bg-gradient-to-tl"
         )}
         suppressHydrationWarning>
         <ThemeProvider
